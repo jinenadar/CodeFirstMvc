36f319a098951519ee73272d80b7281ac2abd630
DELL <DELL@DESKTOP-FO6L500>
2022-07-14 20:49:05 +0530

    Add project files.

---
+++ CodeFirstMvc/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMvc.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        
+
+
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+   
+}
